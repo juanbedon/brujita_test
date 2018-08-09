e3b0d82874b79c4a85244c0e438eabe17c1fdc6a
Juan <juanbedon26@hotmail.com>
2018-08-09 15:48:54 -0500

    Added Library model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_09_204435) do
+ActiveRecord::Schema.define(version: 2018_08_09_204759) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,13 @@ ActiveRecord::Schema.define(version: 2018_08_09_204435) do
     t.string "title"
     t.text "description"
     t.string "teacher"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "libraries", force: :cascade do |t|
+    t.integer "course_id"
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
