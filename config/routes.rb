95edb6ef0a9978f589e8c705b84b2b65abc15a1f
Juan <juanbedon26@hotmail.com>
2018-08-09 16:23:32 -0500

    Created pricing and registrations controller and library views. Also added subscription, pricing, registrations and library to the routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,17 @@ require 'sidekiq/web'
 
 Rails.application.routes.draw do
 
-  resources :courses
-  resources :library, only[:index]
-  devise_for :users
+  resources :courses do
+  	member do
+  		put "add", to: "courses#library"
+  		put "remove", to: "courses#library"
+  	end
+  end
+
+  resources :library, only:[:index]
+  resources :pricing, only:[:index]
+  devise_for :users, controllers: { registrations: "registrations" }
   root to: 'courses#index'
+  resources :subscriptions
 
 end
