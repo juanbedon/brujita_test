44655494f3a3e9ca8ab771051652e7c9c2f3a83d
Juan <juanbedon26@hotmail.com>
2018-08-09 16:47:47 -0500

    Added helpers to application and courses and modified layout views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+
+	def current_user_subscribed?
+		user_signed_in? && current_user.subscribed?
+	end
+
+	def admin?
+		user_signed_in? && current_user.admin?		
+	end
+
+	def subscribed?
+		user_signed_in? && current_user.subscribed?
+	end
+
+	def titleize(str)
+		str.gsub('_',' ').capitalize
+	end
+
 end
--- app/helpers/courses_helper.rb
+++ app/helpers/courses_helper.rb
@@ -1,2 +1,7 @@
 module CoursesHelper
+
+	def user_add_to_library? user, course
+		user.libraries.where(user: user, book: book).any?
+	end
+
 end
