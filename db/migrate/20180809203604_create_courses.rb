cd06d3069e4a11057ee539806da07fe887d7fac1
Juan <juanbedon26@hotmail.com>
2018-08-09 15:38:05 -0500

    Added minimagick and stripe to the gemfile. Created Course Model

---
+++ db/migrate/20180809203604_create_courses.rb
@@ -0,0 +1,12 @@
+class CreateCourses < ActiveRecord::Migration[5.2]
+  def change
+    create_table :courses do |t|
+      t.string :title
+      t.text :description
+      t.string :teacher
+      t.integer :user_id
+
+      t.timestamps
+    end
+  end
+end
