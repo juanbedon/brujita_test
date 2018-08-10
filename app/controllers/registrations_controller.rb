10e81c4efe2343f2b0ae42241dce4636da407960
Juan <juanbedon26@hotmail.com>
2018-08-10 16:15:54 -0500

    Added redirect_to

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,6 +4,7 @@ class RegistrationsController < Devise::RegistrationsController
 
 		def after_sign_up_path_for(resource)
 			'/pricing'
+			redirect_to library_index_path, notice: "Welcome."
 		end
 
 end
