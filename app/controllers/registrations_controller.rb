cd5f88381e932a23752e9e4279363aa3d728b27a
Juan <juanbedon26@hotmail.com>
2018-08-11 10:49:32 -0500

    Removed redirect_to after signin up

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,7 +4,6 @@ class RegistrationsController < Devise::RegistrationsController
 
 		def after_sign_up_path_for(resource)
 			'/pricing'
-			redirect_to library_index_path, notice: "Welcome."
 		end
 
 end
